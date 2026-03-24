78ea5a811eb27bd610c6287f7182fb9530c3e1d9
NIKHIL RAY <nikhil.ray1402@gmail.com>
2026-03-24 14:13:05 +0530

    Time: 10 ms (52.54%), Space: 179.5 MB (22.23%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
@@ -3,9 +3,7 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int[] rowOnes = new int[m];
-        int[] rowZeros = new int[m];
         int[] colOnes = new int[n];
-        int[] colZeros = new int[n];
         int res[][]=new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -13,15 +11,13 @@ class Solution {
                     rowOnes[i]++;
                     colOnes[j]++;
                 }
-                if(grid[i][j]==0){
-                    rowZeros[i]++;
-                    colZeros[j]++;
-                }
             }
         }
             for(int i=0;i<m;i++){
                for(int j=0;j<n;j++){
-                res[i][j]=rowOnes[i]+colOnes[j]-rowZeros[i]-colZeros[j];
+                int rowZeros=m-rowOnes[i];
+                int colZeros=n-colOnes[j];
+                res[i][j]=rowOnes[i]+colOnes[j]-rowZeros-colZeros;
             }
          }
          return res;
